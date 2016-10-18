1bcf67b9e0c96321bb18cb5881a42165caea4efd
Alexander Khotemskoy <Xotabu4@gmail.com>
2016-10-18 20:52:49 +0300

    Added wait for multiple promise
    Added wait for multiple promise

--- specs/search_spec.js
+++ specs/search_spec.js
@@ -5,11 +5,12 @@ let HomePage = require('../src/pageObjects/HomePage');
 let SearchPage = require('../src/pageObjects/SearchPage');
 
 
+
 describe(`Searching: `, function (){
     let homePage = new HomePage();
     let searchPage = new SearchPage();
 
-    it('simple search should be successful', function () {
+    xit('simple search should be successful', function () {
         let searchRequest = 'Test';
         homePage.search(searchRequest);
 
@@ -19,5 +20,32 @@ describe(`Searching: `, function (){
         expect(searchPage.getHeaderTitle())
             .toBe(searchRequest, 'Page title should contain search request');
     });
+
+    xit('How to handle and log errors with promises', function () {
+        let log = (error)=> {
+            console.log('OLOLO PAGE CRASHED!', error);
+            throw Error(`Page open was crashed`);
+        };
+        browser.get('http://as234234ldkfj.com').then(undefined,log); 
+    });
+
+    it('How to wait for multiple promises to resolve', function () {
+        let firstTrack = homePage.tracks(0).getTrackInfo()
+        let secondTrack = homePage.tracks(1).getTrackInfo()
+        
+        protractor.promise.all([firstTrack, secondTrack]).then(function (texts) {
+            console.log('firstTrack', texts[0]);
+            console.log('secondTrack', texts[1]);
+            expect(texts[0]).not.toBe(texts[1]);
+        })
+
+        firstTrack.then(function (firstTrackText) {
+            secondTrack.then(function (secondTrackText) {
+                expect(firstTrackText).not.toBe(secondTrackText)
+            })
+
+        })
+
+    }); 
     
 });
